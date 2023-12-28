e8618499267e279c60b6c212bd5f3b646490f659
Victor2022246 <146198699+SanderSSoares@users.noreply.github.com>
2023-12-28 11:00:33 +0000

    Commenting database.js
    Commenting database.js file and adding considerations we have made

--- database.js
+++ database.js
@@ -1,5 +1,8 @@
+//Importing mysql module to use its functionality for working with MYSQL databases
 var mysql = require("mysql");
-
+//Using the create connection method to create a connection object with MYSQL database
+//connection details (host, database name, user, and password)
+//All these credentials prior defined on the MYSQL workbench
 var connection = mysql.createConnection({
     host: "localhost",
     database:"mysql_db",
@@ -7,5 +10,6 @@ var connection = mysql.createConnection({
     password:"password"
 
 })
-
+//Exportting the connection object so that it can be imported and used in other parts of our peoject
+//The same connection is called on server.js to allow connectivity
 module.exports = connection;
